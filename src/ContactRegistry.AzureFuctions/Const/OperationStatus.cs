a61bbcaf9034a880900f4d85915b661315d5c5dd
Dibeh, Marc <mdibeh@kpmg.fr>
2024-09-30 15:22:56 +0200

    #337547

---
+++ src/ContactRegistry.AzureFuctions/Const/OperationStatus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactRegistry.AzureFuctions.Const
+{
+    /// <summary>
+    /// consts for Operation status.
+    /// </summary>
+    public static class OperationStatus
+    {
+        /// <summary>
+        /// Operation with status Approvced.
+        /// </summary>
+        public const string Approved = nameof(Approved);
+
+        /// <summary>
+        /// Operation with status Refused.
+        /// </summary>
+        public const string Refused = nameof(Refused);
+
+        /// <summary>
+        /// Operation with status Pending.
+        /// </summary>
+        public const string Pending = nameof(Pending);
+    }
+}
