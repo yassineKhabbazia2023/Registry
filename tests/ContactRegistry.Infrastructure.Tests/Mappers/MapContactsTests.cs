4f552789ff9997cd42dd54fd2e4ec08331cac496
Fory, Clement <cfory@kpmg.fr>
2024-10-29 16:29:45 +0100

    TU

---
+++ tests/ContactRegistry.Infrastructure.Tests/Mappers/MapContactsTests.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="MapContactsTests.cs" company="Pulse">
+// Copyright (c) Pulse. All rights reserved.
+// </copyright>
+
+using Application.Models;
+using AutoFixture;
+using Infrastructure.Mappers;
+
+namespace ContactRegistry.Infrastructure.Tests.Mappers;
+
+public class MapContactsTests
+{
+    private readonly Fixture _fixture;
+
+    public MapContactsTests()
+    {
+        _fixture = new Fixture();
+        _fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList().ForEach(b => _fixture.Behaviors.Remove(b));
+        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+    }
+
+    [Fact]
+    public void MapContactCsvToContactEntity_ShouldMapsCorrectly()
+    {
+        var contact = _fixture.Create<RefContactCsv>();
+
+        var result = contact.MapContactCsvToContactEntity();
+
+        Assert.NotNull(result);
+        Assert.Equal(contact.ContactFlagStatus, result.ContactFlagStatus);
+        Assert.Equal(contact.Email, result.Email);
+        Assert.Equal(contact.FirstName, result.FirstName);
+        Assert.Equal(contact.LastName, result.LastName);
+        Assert.Equal(contact.IsCustomer, result.IsCustomer);
+        Assert.Equal(contact.LandPhone, result.LandPhone);
+        Assert.Equal(contact.MobilePhone, result.MobilePhone);
+        Assert.Equal(contact.JobDescription, result.JobDescription);
+        Assert.Equal(contact.OfficeId, result.OfficeId);
+        Assert.Equal(contact.Operation, result.OperationType);
+    }
+
+    [Fact]
+    public void MapContactCsvToContactEntity_WithNullSource_ShouldReturnNull()
+    {
+        var result = MapContacts.MapContactCsvToContactEntity(null!);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void MapContactCsvsToContactEntities_ShouldMapsCorrectly()
+    {
+        var contacts = _fixture.CreateMany<RefContactCsv>(2);
+
+        var result = contacts.MapContactCsvsToContactEntities();
+
+        Assert.NotNull(result);
+        Assert.NotEmpty(result);
+        Assert.Equal(contacts.Count(), result.Count());
+    }
+
+    [Fact]
+    public void MapContactCsvsToContactEntities_WithNullSource_ShouldReturnEmptyList()
+    {
+        var result = MapContacts.MapContactCsvsToContactEntities(null!);
+
+        Assert.Empty(result);
+    }
+}
---
+++ tests/ContactRegistry.Infrastructure.Tests/Repository/RoleRepositoryTests.cs
@@ -0,0 +1,9 @@
+﻿// <copyright file="RoleRepositoryTests.cs" company="Pulse">
+// Copyright (c) Pulse. All rights reserved.
+// </copyright>
+
+namespace ContactRegistry.Infrastructure.Tests.Repository;
+
+public class RoleRepositoryTests
+{
+}
