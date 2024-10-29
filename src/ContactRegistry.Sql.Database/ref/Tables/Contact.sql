46d3bdb5a390200838ac55d094d2510d415cb116
Fory, Clement <cfory@kpmg.fr>
2024-10-29 09:39:15 +0100

    feat(sql): ajout des tables Ref

---
+++ src/ContactRegistry.Sql.Database/ref/Tables/Contact.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [ref].[Contact](
+	[ContactFlagStatus] [int] NOT NULL,
+	[Email] [nvarchar](255) NOT NULL,
+	[FirstName] [nvarchar](255) NOT NULL,
+	[LastName] [nvarchar](255) NOT NULL,
+	[IsCustomer] [bit] NOT NULL,
+	[LandPhone] [nvarchar](255) NULL,
+	[MobilePhone] [nvarchar](255) NULL,
+	[JobDescription] [nvarchar](255) NULL,
+	[OfficeId] [uniqueidentifier] NULL,
+	[Operation] [nvarchar](20) NOT NULL
+)
+GO
+
---
+++ src/ContactRegistry.Sql.Database/ref/Tables/Role.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [ref].[Role](
+    [ContactEmail] NVARCHAR(255) NOT NULL,
+    [AccountNumber] NVARCHAR(50) NOT NULL,
+    [RoleFlagStatus] [int] NOT NULL,
+    [Description] [nvarchar](1000) NULL,
+)
+GO
