cfe91934b2eabd34cb0481d72717c5a346e06b1b
BEN AMOR, Oussema <obenamor@rydge.fr>
2025-03-17 14:12:56 +0000

    Merged PR 3220: chore: set first name and lastname nullable
    chore: set first name and lastname nullable
    
    Related work items: #44353, #44355

--- src/Registry.Sql.Database/Contact/Tables/Contacts.sql
+++ src/Registry.Sql.Database/Contact/Tables/Contacts.sql
@@ -4,7 +4,7 @@
 	[ContactGlobalUniqueId]	UNIQUEIDENTIFIER	NULL,
 	[Email]     			VARCHAR(250)		NOT NULL,
 	[Type]                  VARCHAR(20)         NOT NULL,
-	[FirstName]				VARCHAR(250)			NOT NULL,
-	[LastName]			VARCHAR(250)			NOT NULL,
+	[FirstName]				VARCHAR(250)		NULL,
+	[LastName]				VARCHAR(250)		NULL,
     CONSTRAINT [C_Contact_PK] PRIMARY KEY CLUSTERED ([ContactId] ASC)
 )
